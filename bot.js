cac7697aefc45a4e43eb0dbaddfb656cd8a059ff
Milan-aka-Suguru <122789724+Milan-aka-Suguru@users.noreply.github.com>
2025-05-30 21:07:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,11 +12,9 @@ const client = new Client({
     ] ,
     partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
-client.once('ready', () => {
-    console.log('Bot is online!');
-    setInterval(async () => {
+async function changeName(){
         try {
-            const server = await MinecraftServer.status(process.env.MCSERVERIP, parseInt(process.env.MCSERVERPORT));
+            const server = await MinecraftServer.status(process.env.MCSERVERIP, parseInt(process.env.MCSERVERPORT), { timeout: 10000 } );
             const newUsername = server.motd.clean;
             if (client.user.username !== newUsername) {
                 await client.user.setUsername(newUsername);
@@ -25,14 +23,24 @@ client.once('ready', () => {
         } catch (error) {
             console.log("Failed to fetch server MOTD or set username:", error.message);
         }
-    }, 1000 * 60 * 30); // every 30 minutes (safe for Discord's rate limit)
-    setInterval(async () => {
-        try {
-            const server = await MinecraftServer.status(process.env.MCSERVERIP, parseInt(process.env.MCSERVERPORT));
+}
+async function changeStatus(){
+    try {
+            const server = await MinecraftServer.status(process.env.MCSERVERIP, parseInt(process.env.MCSERVERPORT), { timeout: 10000 } );
             client.user.setActivity({name: 'ONLINE || Player count: '+server.players.online, type: ActivityType.Custom });           
         } catch (error) {            
             client.user.setActivity({name: 'OFFLINE', type: ActivityType.Custom });
         }
+    } 
+client.once('ready', () => {
+    console.log('Bot is online!');
+    changeName();
+    setInterval(async () => {
+        changeName();
+    }, 1000 * 60 * 30); // every 30 minutes (safe for Discord's rate limit)
+    changeStatus();
+    setInterval(async () => {
+        changeStatus();
     },1000 * parseInt(process.env.MCSERVERSTATUSUPDATEINTERVAL) || 60000*5); // Default to 5 min if not set
 });
 const rest = new REST({ version: '10' }).setToken(process.env.BOTTOKEN);
@@ -52,16 +60,18 @@ const commands = [
     }
 })();
 client.on('interactionCreate', async (interaction) => {
+         //interaction.deferReply({ flags: 64});
     if(interaction.isCommand()) {
         const { commandName } = interaction;
         if (commandName === 'serverstatus') {
-        await interaction.deferReply({ flags:64 });
+
         try {            
-            const server = await MinecraftServer.status(process.env.MCSERVERIP, parseInt(process.env.MCSERVERPORT));
-            console.log(server);
-            await interaction.editReply({ content: `Server Status: **ONLINE**\nPlayer Count: **${server.players.online}**`, flags:64 });            
+            const server = await MinecraftServer.status(process.env.MCSERVERIP, parseInt(process.env.MCSERVERPORT), { timeout: 10000 } );
+             console.log(server);
+             await interaction.reply({ content: `Server Status: **ONLINE**\nPlayer Count: **${server.players.online}**`, flags: 64});            
         } catch (error) {  
-            await interaction.editReply({ content: `Server Status: **OFFLINE**\ncode: ${error.code}`, flags:64 });            
+            console.log(error);
+            await interaction.reply({ content: `Server Status: **OFFLINE**\ncode: ${error.code}`, flags: 64});            
         }            
             return;
 }}});
